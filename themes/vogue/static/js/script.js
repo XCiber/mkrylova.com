e729e3e66ace347ac4233144ff8b526a6e67ce0b
Andrey S. Kolesnichenko <andrey@kolesnichenko.com>
2015-10-20 13:17:21 +0300

    Initail commit
    First draft with cariusel

---
+++ themes/vogue/static/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by xciber on 20.10.15.
+ */
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        margin:10,
+        loop:true,
+        height:300px,
+        autoWidth:true,
+        items:4
+    })
+});
